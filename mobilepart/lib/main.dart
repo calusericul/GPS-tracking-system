34cfd5d27ff6e1f596f96d1ea7090cb94e614d7d
Mihai Vasile Caluser <mihai.caluser@yahoo.com>
2021-12-29 13:32:14 +0200

    mobile now works

--- mobilepart/lib/main.dart
+++ mobilepart/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'DataModel.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MaterialApp(
@@ -7,20 +11,33 @@ void main() {
   ));
 }
 
-class Location {
-  late int id;
-  late String latitude;
-  late String longitude;
-  late String terminal_id;
-
-}
-
 class Gps extends StatefulWidget {
   @override
   State<Gps> createState() => _GpsState();
 }
 
+Future<http.Response> submitData(
+    String terminalId, String latitude, String longitude) {
+
+  Map<String, dynamic> body = {
+    'terminalId': terminalId,
+    'latitude': latitude,
+    'longitude': longitude
+  };
+
+  return http.post(
+      Uri.parse("http://10.0.2.2:8081/positions/position"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: json.encode(body));
+}
+
 class _GpsState extends State<Gps> {
+  TextEditingController terminalIdController = TextEditingController();
+  TextEditingController latitudeController = TextEditingController();
+  TextEditingController longitudeController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,17 +52,25 @@ class _GpsState extends State<Gps> {
             TextField(
                 onChanged: (text) {},
                 decoration: InputDecoration(hintText: "Enter terminal id"),
-                keyboardType: TextInputType.number),
+                keyboardType: TextInputType.number,
+                controller: terminalIdController),
             TextField(
                 onChanged: (text) {},
                 decoration: InputDecoration(hintText: "Enter latitude"),
-                keyboardType: TextInputType.number),
+                keyboardType: TextInputType.number,
+                controller: latitudeController),
             TextField(
                 onChanged: (text) {},
                 decoration: InputDecoration(hintText: "Enter longitude"),
-                keyboardType: TextInputType.number),
+                keyboardType: TextInputType.number,
+                controller: longitudeController),
             FlatButton(
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  submitData(terminalIdController.text, latitudeController.text,
+                      longitudeController.text);
+                });
+              },
               child: Text("Submit marker"),
               color: Colors.red,
             ),
